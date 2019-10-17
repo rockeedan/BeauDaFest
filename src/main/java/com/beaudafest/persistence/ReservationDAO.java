f5aed5de9bc51f9d0268ccc5156c20ef79d02e67
고솔비 <gosolb0904@naver.com>
2019-10-17 19:18:15 +0900

    selectMonthlyRsvnCount dao, service 추가

--- src/main/java/com/beaudafest/persistence/ReservationDAO.java
+++ src/main/java/com/beaudafest/persistence/ReservationDAO.java
@@ -2,6 +2,8 @@ package com.beaudafest.persistence;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
+
 
 
 
@@ -9,6 +11,10 @@ public interface ReservationDAO {
 	
 	//예약 리스트 조회
 	public List<Map<String, Object>> selectRsvnList(int shopnum);
+	
+	//area chart 조회
+	public List<Map<String, Object>> selectMonthlyRsvnCount(int shopnum);
+
 	
 	
 
