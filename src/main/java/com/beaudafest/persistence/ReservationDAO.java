6124b68123c275a1c40202b91170f12a8b90d458
고솔비 <gosolb0904@naver.com>
2019-10-22 15:11:34 +0900

    reservation dao,service 추가

--- src/main/java/com/beaudafest/persistence/ReservationDAO.java
+++ src/main/java/com/beaudafest/persistence/ReservationDAO.java
@@ -12,9 +12,11 @@ public interface ReservationDAO {
 	//예약 리스트 조회
 	public List<Map<String, Object>> selectRsvnList(int shopnum);
 	
-	//area chart 조회
-	public List<Map<String, Object>> selectMonthlyRsvnCount(int shopnum);
+	//월별 총 수익, area chart 조회
+	public List<Map<String, Object>> selectMonthlyRsvnProfits(int shopnum);
 
+	//월별 예약 수, bar chart 조회
+	public List<Map<String, Object>> selectMonthlyRsvnCount(int shopnum);
 	
 	
 
