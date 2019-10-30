c384d95b71e7d66a00f9705d1abfe60295a16bbc
HANA <45937565+yuhn126@users.noreply.github.com>
2019-10-30 16:27:20 +0900

    오류 수정

--- src/main/java/com/beaudafest/service/ReservationServiceImpl.java
+++ src/main/java/com/beaudafest/service/ReservationServiceImpl.java
@@ -1,21 +1,19 @@
 package com.beaudafest.service;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
 
-import org.omg.CORBA.TRANSACTION_ROLLEDBACK;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.TransactionManagementConfigurationSelector;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import com.beaudafest.persistence.ReservationDAO;
+
+
 @Service
 public class ReservationServiceImpl implements ReservationService{
 
