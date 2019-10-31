91d8d0270944a167f78019f0f618854a57f2a8f8
고솔비 <gosolb0904@naver.com>
2019-10-31 15:46:51 +0900

    no message

--- src/main/java/com/beaudafest/controller/myPageController.java
+++ src/main/java/com/beaudafest/controller/myPageController.java
@@ -1,14 +1,10 @@
 package com.beaudafest.controller;
 
-import java.util.List;
-import java.util.Map;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.beaudafest.service.ReservationService;
